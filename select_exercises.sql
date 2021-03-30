6e91ba904d96c347fb59a99ac6673a0d33a72647
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-03-30 15:06:04 -0500

    practicing SELECT

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,1 +1,25 @@
 USE codeup_test_db;
+
+SELECT name_of_album
+    AS 'Albums from Pink Floyd'
+    FROM albums
+    WHERE artist = "Pink Floyd";
+SELECT release_date
+    AS 'Sgt. Peppers Lonely Hearts Club Band was released in '
+    FROM albums
+    WHERE name_of_album LIKE "Sgt. Peppers%";
+SELECT genre
+    AS 'The genre for Nevermind'
+    FROM albums
+    WHERE name_of_album = "Nevermind";
+SELECT name_of_album
+    AS 'Albums released in 1990'
+    FROM albums
+    WHERE release_date BETWEEN 1990 AND 1999;
+SELECT name_of_album
+    AS 'ALL Rock albums'
+    FROM albums
+    WHERE genre LIKE "%rock%";
+
+
+
