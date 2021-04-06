c14f2b879bae0deec7069f51f73bbcfb4a94a45d
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-04-06 16:27:28 -0500

    extra practice for relationships in mysql

---
+++ join_subqueries_extra.sql
@@ -0,0 +1,15 @@
+use employees;
+-- what is the most common birthday in the company? least common?
+-- SELECT COUNT(birth_date), birth_date AS 'Most Common Birthdays'
+--     from employees
+--     where emp
+
+
+-- what is the average salary of managers by department?
+
+
+-- how many employees currently work in each department?
+
+-- what is the average salary for each department?
+
+-- what was the average salary in the 80s? By department?
